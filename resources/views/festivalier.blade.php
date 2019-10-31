1d9c5a53b3744cad2ba8ec6684ff49afc1ccc620
chauselmann <charlie@cosavostra.com>
2019-10-31 15:45:21 +0100

    update 2

--- resources/views/festivalier.blade.php
+++ resources/views/festivalier.blade.php
@@ -20,26 +20,26 @@
     <div class="row countdown row-text">
       <div class="square">
         <div class="round">
-          <p>JOURS</p>
           <p id="days"></p>
+          <p>jours</p>
         </div>
       </div>
       <div class="square">
         <div class="round">
-          <p>HEURES</p>
           <p id="hours"></p>
+          <p>heures</p>
         </div>
       </div>
       <div class="square">
         <div class="round">
-          <p>MINUTES</p>
           <p id="minutes"></p>
+          <p>minutes</p>
         </div>
       </div>
       <div class="square">
         <div class="round">
-          <p>SECONDES</p>
           <p id="seconds"></p>
+          <p>secondes</p>
         </div>
       </div>
     </div>
@@ -48,6 +48,96 @@
     </div>
     <div class="row row-text d-flex justify-content-center r-second r-promo">
       <p>Vote dès maintenant pour obtenir un code promo sur ta place de concert </p>
+    </div>
+    <div class="row row-section">
+      <div class="col-md-8 col-image">
+        <div class="img-section">
+          <img src="@asset('images/pop.jpg')">
+          <div class="white-rectangle"></div>
+        </div>
+      </div>
+      <div class="col-md-4 col-btn">
+        <h2>ROCK</h2>
+        <hr>
+        <div class="custom-btn">
+          <a href="">Voir les vidéos</a>
+          <div class="background"></div>
+        </div>
+      </div>
+    </div>
+    <div class="row row-section reverse">
+      <div class="col-md-4 col-btn">
+        <h2>ROCK</h2>
+        <hr>
+        <div class="custom-btn">
+          <a href="">Voir les vidéos</a>
+          <div class="background"></div>
+        </div>
+      </div>
+      <div class="col-md-8 col-image">
+        <div class="img-section">
+          <img src="@asset('images/pop.jpg')">
+          <div class="white-rectangle"></div>
+        </div>
+      </div>
+    </div>
+    <div class="row row-section">
+      <div class="col-md-8 col-image">
+        <div class="img-section">
+          <img src="@asset('images/pop.jpg')">
+          <div class="white-rectangle"></div>
+        </div>
+      </div>
+      <div class="col-md-4 col-btn">
+        <h2>ROCK</h2>
+        <hr>
+        <div class="custom-btn">
+          <a href="">Voir les vidéos</a>
+          <div class="background"></div>
+        </div>
+      </div>
+    </div>
+    <div class="row row-section reverse">
+      <div class="col-md-4 col-btn">
+        <h2>ROCK</h2>
+        <hr>
+        <div class="custom-btn">
+          <a href="">Voir les vidéos</a>
+          <div class="background"></div>
+        </div>
+      </div>
+      <div class="col-md-8 col-image">
+        <div class="img-section">
+          <img src="@asset('images/pop.jpg')">
+          <div class="white-rectangle"></div>
+        </div>
+      </div>
+    </div>
+    <div class="row row-section">
+      <div class="col-md-8 col-image">
+        <div class="img-section">
+          <img src="@asset('images/pop.jpg')">
+          <div class="white-rectangle"></div>
+        </div>
+      </div>
+      <div class="col-md-4 col-btn">
+        <h2>ROCK</h2>
+        <hr>
+        <div class="custom-btn">
+          <a href="">Voir les vidéos</a>
+          <div class="background"></div>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-12 coll-img-full">
+        <div class="img-section">
+          <img src="@asset('images/pop.jpg')" width="100%" height="100%">
+          <div class="white-rectangle">
+            <p class="underline--magical">Découvrez le jury</p>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 @endsection
--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,5 +1,12 @@
-<footer class="content-info">
-  <div class="container">
-    @php dynamic_sidebar('sidebar-footer') @endphp
-  </div>
+<footer class=" container content-info">
+    <div class="row">
+      <div class="col-md-6 row-left">
+        <a href="#">Mentions légales</a>
+        <p>Copyright Make Your Fest 2019</p>
+      </div>
+      <div class="col-md-6 row-rs">
+        <img src="@asset('images/facebook-logo.svg')">
+        <img src="@asset('images/instagram.svg')">
+      </div>
+    </div>
 </footer>
